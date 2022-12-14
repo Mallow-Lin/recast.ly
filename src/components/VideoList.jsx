05cdcc5877d8a678b2cbf81fee2643438422c3c5
Anthony Bui <anthonyb50@gmail.com>
2022-12-13 23:35:39 -0500

    Partial completion

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,12 @@
-var VideoList = () => (
+var VideoList = (props) => ( // interate over the array (map)
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div>{props.videos.map((video) => (<VideoListEntry video={video} />))}</div>
   </div>
 );
+
+//<div><h5><em>props.videos.map</em> view goes here</h5></div>
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
